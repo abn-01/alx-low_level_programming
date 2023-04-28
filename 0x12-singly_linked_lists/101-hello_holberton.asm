34725cb183f80969a21fc1f8b0eb5b38335ddfcc
abn-01 <ogegaabner@gmail.com>
2023-04-28 13:53:34 -0700

    Hello

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+section .data
+    hello db "Hello, Holberton",0
+    fmt db "%s\n",0
+
+section .text
+    global main
+
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    push hello
+    push fmt
+    call printf
+    add rsp, 16
+
+    mov rsp, rbp
+    pop rbp
+    mov eax, 0
+    ret
