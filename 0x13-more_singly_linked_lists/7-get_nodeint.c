5ec642cd6a0943db2a2e69f1c0fb3001901c88b3
abn-01 <ogegaabner@gmail.com>
2023-05-02 03:57:45 -0700

    hello 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list,
+ * and returns the head node's data (n)
+ * @head: double pointer to the head of the linked list
+ *
+ * Return: the head node's data (n) or 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int data = 0;
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return (data);
+
+	temp = *head;
+	data = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (data);
+}
